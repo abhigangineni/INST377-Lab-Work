741e84d750d8b7bf3f77d456d9c149cc24c9e65f
abhigangineni <abhigangineni@hotmail.com>
2022-09-29 10:15:12 -0400

    Added move right function

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -71,8 +71,14 @@ function undraw() {
 
 // freeze function
 function freeze() {
-  if (current.some((index) => squares[currentPosition + index + width].classList.contains("taken"))) {
-    current.forEach((index) => squares[currentPosition + index].classList.add("taken"));
+  if (
+    current.some((index) =>
+      squares[currentPosition + index + width].classList.contains("taken")
+    )
+  ) {
+    current.forEach((index) =>
+      squares[currentPosition + index].classList.add("taken")
+    );
     // Start a new tetromino falling
     random = Math.floor(Math.random() * theTetrominos.length);
     current = theTetrominos[random][currentRotation];
@@ -92,12 +98,28 @@ function moveDown() {
 // move tetromino to the left unless hits edge or blockage
 function moveLeft() {
   undraw();
-  const isAtLeftEdge = current.some((index) => (currentPosition + index) % width === 0);
+  const isAtLeftEdge = current.some((index) => (currentPosition + index) % width === width-1);
 
   if (!isAtLeftEdge) currentPosition -= 1;
 
-  if (current.some((index) => squares[currentPosition + index].classList.contains("taken"))) {
+  if (current.some((index) => squares[currentPosition + index].classList.contains('taken'))) {
     currentPosition += 1;
+  }
+
+  draw();
+}
+
+// move tetromino to the right unless hits edge or blockage
+function moveRight() {
+  undraw();
+  const isAtRightEdge = current.some(
+    (index) => (currentPosition + index) % width === 0
+  );
+
+  if (!isAtRightEdge) currentPosition += 1;
+
+  if (current.some((index) => squares[currentPosition + index].classList.contains('taken'))) {
+    currentPosition -= 1;
   }
 
   draw();
@@ -110,7 +132,13 @@ timerId = setInterval(moveDown, 1000);
 function control(e) {
   if (e.keyCode === 37) {
     moveLeft();
+  } else if (e.keyCode === 38) {
+    rotate();
+  } else if (e.keyCode === 39) {
+    moveRight();
+  } else if (e.keyCode === 40) {
+    moveDown();
   }
 }
 
-document.addEventListener('keyup', control)
+document.addEventListener("keyup", control);
