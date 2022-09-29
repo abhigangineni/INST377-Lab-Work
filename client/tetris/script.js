66515fadf776e5793692c6b6a9efce4655c05288
abhigangineni <abhigangineni@hotmail.com>
2022-09-29 08:49:30 -0400

    Linked button and score to script file

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector('.grid');
 const squares = Array.from(document.querySelectorAll('.grid div'));
+const ScoreDisplay = document.querySelector('#score');
+const StartBtn = document.querySelector('#start-button');
 const width = 10;
-
-console.log(squares);
