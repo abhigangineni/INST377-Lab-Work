a287c0f5a41edea878f8928d3d805de1a856d649
abhigangineni <abhigangineni@hotmail.com>
2022-11-18 20:40:19 -0500

    Fixed map centering

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -104,7 +104,7 @@ function markerPlace(array, map) {
     const {coordinates} = item.geocoded_column_1;
     L.marker([coordinates[1], coordinates[0]]).addTo(map);
     if (index === 0) {
-      map.setView([coordinates[1], coordinates[0]], 12);
+      map.setView([coordinates[1], coordinates[0]], 9.5);
     }
   });
 }
